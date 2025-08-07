ba73b73b2b7f33227cec07f23282a8ac7be87011
Geodev122 <geo.elnajjar@gmail.com>
2025-08-07 12:09:41 +0300

    Remove extra closing div tag in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -56,7 +56,6 @@ export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
             </div>
           </div>
         </div>
-        </div>
       </header>
 
       <main className="max-w-7xl mx-auto py-4 sm:py-6 px-3 sm:px-6 lg:px-8">
